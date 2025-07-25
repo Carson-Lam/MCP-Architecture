de9bf48999f346f4bfeea2508ddf9b49e06d483b
Linda LIU <lamcn51@gmail.com>
2025-07-25 15:36:37 +0800

    yes

--- MCP-Server/src/main/java/com/example/demo/StockApplication.java
+++ MCP-Server/src/main/java/com/example/demo/StockApplication.java
@@ -1,12 +1,7 @@
 package com.example.demo;
 
-import java.util.List;
-
-import org.springframework.ai.support.ToolCallbacks;
-import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,12 +13,13 @@ public class StockApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplication.class, args);
 		StockService ss = new StockService();
+		System.out.println(ss.getStocks());
 		
 	}
 
 	@Bean
 	public ToolCallbackProvider stockTools(StockService stockService) {
-		return  MethodToolCallbackProvider.builder().toolObjects(stockService).build();
+		return MethodToolCallbackProvider.builder().toolObjects(stockService).build();
 	}
 
 }
