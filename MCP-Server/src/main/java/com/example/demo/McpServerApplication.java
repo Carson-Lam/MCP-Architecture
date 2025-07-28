7b7412ac78ef2b66d6d5e4ec034d74835170069a
Linda LIU <lamcn51@gmail.com>
2025-07-28 09:35:18 +0800

    Merged functionalities

--- MCP-Server/src/main/java/com/example/demo/McpServerApplication.java
+++ MCP-Server/src/main/java/com/example/demo/McpServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -13,10 +14,19 @@ public class McpServerApplication {
 
 		WeatherService ws = new WeatherService();
 		System.out.println(ws.getWeatherForecastByLocation(33.7490, -84.3880));
+		
+		StockService ss = SpringApplication.run(McpServerApplication.class, args).getBean(StockService.class);
+		System.out.println(ss.getStocks());
+	
 	}
 
 	@Bean
 	public ToolCallbackProvider weatherTools(WeatherService weatherService) {
 		return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();	
+	}
+
+	@Bean
+	public ToolCallbackProvider stockTools(StockService stockService) {
+		return MethodToolCallbackProvider.builder().toolObjects(stockService).build();
 	}
 }
---
+++ MCP-Server/src/main/java/com/example/demo/StockService.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class StockService{
+
+    //Stock class
+    public record Stock(
+        String code, 
+        String url
+    ){}
+
+    //LogFactory interface, list interface
+    private static final Logger LOG = LoggerFactory.getLogger(StockService.class);
+    private List<Stock> stocks = new ArrayList<>();   
+
+    //Tool for getting list of courses
+    @Tool(name = "CN_get_stocks", description="gets a list of Chinese stock quotes with largest market cap")
+    public List<Stock> getStocks() {
+        // System.err.println("DEBUG: getStocks() method called");
+        return stocks;
+    } 
+
+    //Tool for getting a single course
+    @Tool(name = "CN_get_stock", description = "get a single Chinese stock quote by stock code")
+    public Optional<Stock> getStock(String name){
+        // System.err.println("DEBUG: getCourse() method called with title: " + title);
+        return stocks.stream()
+            .filter(stock -> stock.code().equals(name))
+            .findFirst();
+    }
+
+    @PostConstruct //Tags constructor to run after all beans initialized
+    public void init() {
+        // System.err.println("DEBUG: CourseService initialized with courses");
+        stocks.addAll(List.of(
+            new Stock("601398", "https://quote.eastmoney.com/sh601398.html"),
+            new Stock("601288", "https://quote.eastmoney.com/sh601288.html"),
+            new Stock("601939", "https://quote.eastmoney.com/sh601939.html"),
+            new Stock("600941", "https://quote.eastmoney.com/sh600941.html"),
+            new Stock("601988", "https://quote.eastmoney.com/sh601988.html")
+        ));
+    }
+}
